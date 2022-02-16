299243ab4da9bc53a32f52e9d611d53e67fdf2a8
Mohamed Oun <mohamed3on@gmail.com>
2022-02-16 09:51:36 +0200

    show letterBoxed calculated score

--- extension/content-script.js
+++ extension/content-script.js
@@ -7,20 +7,41 @@ const addCommas = (x) => {
 }),
   false;
 
-const ratings = Array.from(document.getElementsByClassName('leftAligned'))
-  .slice(1, 11)
-  .map((element) => parseInt(element.textContent.replace(/,/g, '')));
+const run = (ratings) => {
+  const absoluteScore = ratings[9] + ratings[8] - ratings[0] - ratings[1];
 
-const absoluteScore = ratings[0] + ratings[1] - ratings[9] - ratings[8];
+  const sum = ratings.reduce((a, b) => a + b, 0);
+  const ratio = absoluteScore / sum;
 
-const sum = ratings.reduce((a, b) => a + b, 0);
-const ratio = absoluteScore / sum;
+  const calculatedScore = Math.round(absoluteScore * ratio);
 
-const calculatedScore = Math.round(absoluteScore * ratio);
+  const ScoreElement = document.createElement('div');
+  ScoreElement.innerHTML = `${addCommas(String(calculatedScore))} (${Math.round(ratio * 100)}%)`;
 
-const ScoreElement = document.createElement('div');
-ScoreElement.innerHTML = `${addCommas(String(calculatedScore))} (${Math.round(ratio * 100)}%)`;
-ScoreElement.className = 'sectionHeading';
+  const Headline = document.querySelector('.ratings-histogram-chart  h2 a');
+  ScoreElement.appendAfter(Headline);
+};
 
-const Headline = document.getElementsByClassName('sectionHeading')[0];
-ScoreElement.appendAfter(Headline);
+let hasRun = false;
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (!mutation.addedNodes) {
+      return;
+    }
+
+    const ratingNodes = document.getElementsByClassName('rating-histogram-bar');
+
+    if (ratingNodes.length && !hasRun) {
+      const ratings = Array.from(ratingNodes).map((element) =>
+        parseInt(element.textContent.replace(/,/g, '').split('&')[0])
+      );
+      run(ratings);
+      hasRun = true;
+    }
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
